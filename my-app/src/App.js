c58dfe08ddedca75e54a65ecf244ab11d6a50b1f
cdac@182 <cdac2019>
2020-06-02 16:06:33 +0530

    Frontend Test

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+import './App.css';
+import Gallary from './Component/gallary';
+
+function App() {
+  return (
+    <div className="App">
+      <Gallary/>
+        </div>  );
+}
+
+export default App;
